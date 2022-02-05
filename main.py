456a3d5fd0a59a5c7440f9dae15393e7bcbdafe4
Nastia Zhyryk <nastiazhyryk@gmail.com>
2022-02-05 22:36:13 +0300

    first comit

---
+++ main.py
@@ -0,0 +1,95 @@
+import os
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog, # Диалог открытия файлов (и папок)
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+ 
+app = QApplication([])
+win = QWidget()       
+win.resize(700, 500) 
+win.setWindowTitle('Easy Editor')
+lb_image = QLabel("Картинка")
+btn_dir = QPushButton("Папка")
+lw_files = QListWidget()
+ 
+btn_left = QPushButton("Лево")
+btn_right = QPushButton("Право")
+btn_flip = QPushButton("Зеркало")
+btn_sharp = QPushButton("Резкость")
+btn_bw = QPushButton("Ч/Б")
+ 
+row = QHBoxLayout()          
+col1 = QVBoxLayout()         
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)      
+col1.addWidget(lw_files)     
+col2.addWidget(lb_image, 95) 
+row_tools = QHBoxLayout()    # и строка кнопок
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addLayout(row_tools)
+ 
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+win.setLayout(row)
+ 
+win.show()
+ 
+workdir = ''
+ 
+def filter(files, extensions):
+   result = []
+   for filename in files:
+       for ext in extensions:
+           if filename.endswith(ext):
+               result.append(filename)
+   return result
+ 
+def chooseWorkdir():
+   global workdir
+   workdir = QFileDialog.getExistingDirectory()
+ 
+def showFilenamesList():
+   extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+   chooseWorkdir()
+   filenames = filter(os.listdir(workdir), extensions)
+   lw_files.clear()
+   for filename in filenames:
+       lw_files.addItem(filename)
+
+btn_dir.clicked.connect(showFilenamesList)
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.dir = None 
+        self.filename = None 
+        self.save_dir = "Modifiled/"
+
+    def loadImage(self, dir, filename):
+        self.dir = dir
+        self.filename = filename
+        image_path = os.path.join(dir, filename)
+        self.image = Image.open(image_path)
+
+    def do_bw(self):
+        self.image = self.image.convert("L")
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+    def saveImage(self):
+        path = os.path.join(self.dir, self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
+
+
+win.show()
+app.exec_()
